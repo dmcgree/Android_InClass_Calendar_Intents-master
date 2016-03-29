ffbb12ca8603cdabc648de6ed19346005477c595
dmcgree <dmcgree@css.edu>
2016-03-29 18:53:41 -0500

    final project

--- app/build/generated/source/buildConfig/test/debug/cis3334/simpletext/test/BuildConfig.java
+++ app/build/generated/source/buildConfig/test/debug/cis3334/simpletext/test/BuildConfig.java
@@ -1,7 +1,7 @@
 /**
  * Automatically generated file. DO NOT MODIFY
  */
-package cis3334.simpletext.test;
+package cis3334.McGree_MiniProject.test;
 
 public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
